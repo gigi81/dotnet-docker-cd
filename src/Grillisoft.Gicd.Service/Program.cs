808c41adb2bf332f9dd9cbe67cee1219c8fb0add
Luigi Grilli <luigi.grilli@gmail.com>
2025-12-13 11:55:26 +0100

    WIP

--- src/Grillisoft.Gicd.Service/Program.cs
+++ src/Grillisoft.Gicd.Service/Program.cs
@@ -1,4 +1,13 @@
+using Grillisoft.Gicd;
+using Grillisoft.Gicd.Abstractions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IRepository, Repository>();
+builder.Services.AddSingleton<IStack, Stack>();
+builder.Services.Configure<GithubOptions>(
+    builder.Configuration.GetSection(GithubOptions.SectionName));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
--- src/Grillisoft.Gicd/GithubOptions.cs
+++ src/Grillisoft.Gicd/GithubOptions.cs
@@ -2,6 +2,8 @@ namespace Grillisoft.Gicd;
 
 public class GithubOptions
 {
+    public const string SectionName = "Github";
+    
     public string Token { get; set; } = "";
     public string Owner { get; set; } = "";
     public string RepositoryName { get; set; } = "";
